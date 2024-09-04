d51163312aa078c1b9f0f3353d11807fd71c201e
Alex David Frías <adavidfruiz@gmail.com>
2024-09-04 00:19:36 -0500

    feat: add submission service implementation

---
+++ task-submission-service/src/main/java/com/david/tasksubmissionservice/service/SubmissionServiceImpl.java
@@ -0,0 +1,40 @@
+package com.david.tasksubmissionservice.service;
+
+import com.david.tasksubmissionservice.model.Submission;
+import com.david.tasksubmissionservice.repository.SubmissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubmissionServiceImpl implements SubmissionService {
+
+    @Autowired
+    private SubmissionRepository submissionRepository;
+
+    @Override
+    public Submission submitTask(Long taskId, String githubLink, Long userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Submission getTaskSubmissionById(Long submissionId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Submission> getAllTaskSubmissions() {
+        return null;
+    }
+
+    @Override
+    public List<Submission> getTaskSubmissionsByTaskId(Long taskId) {
+        return null;
+    }
+
+    @Override
+    public Submission acceptDeclineSubmission(Long id, String status) throws Exception {
+        return null;
+    }
+}
