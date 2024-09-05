31ba5a4ac1061dd7eb792dccbbb8035c077f5d52
Alex David Frías <adavidfruiz@gmail.com>
2024-09-05 01:13:17 -0500

    feat: add submission service implementation

--- task-submission-service/src/main/java/com/david/tasksubmissionservice/service/SubmissionServiceImpl.java
+++ task-submission-service/src/main/java/com/david/tasksubmissionservice/service/SubmissionServiceImpl.java
@@ -1,10 +1,12 @@
 package com.david.tasksubmissionservice.service;
 
 import com.david.tasksubmissionservice.model.Submission;
+import com.david.tasksubmissionservice.model.TaskDto;
 import com.david.tasksubmissionservice.repository.SubmissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -13,28 +15,46 @@ public class SubmissionServiceImpl implements SubmissionService {
     @Autowired
     private SubmissionRepository submissionRepository;
 
+    @Autowired
+    private TaskService taskService;
+
     @Override
-    public Submission submitTask(Long taskId, String githubLink, Long userId) throws Exception {
-        return null;
+    public Submission submitTask(Long taskId, String githubLink, Long userId, String jwt) throws Exception {
+        TaskDto task = taskService.getTaskById(taskId, jwt);
+        if (task != null) {
+            Submission submission = new Submission();
+            submission.setTaskId(taskId);
+            submission.setUserId(userId);
+            submission.setGithubLink(githubLink);
+            submission.setSubmissionTime(LocalDateTime.now());
+            return submissionRepository.save(submission);
+        }
+
+        throw new Exception("Task not found with id: " + taskId);
     }
 
     @Override
     public Submission getTaskSubmissionById(Long submissionId) throws Exception {
-        return null;
+        return submissionRepository.findById(submissionId).orElseThrow(() -> new Exception("Task submission not found with id: " + submissionId));
     }
 
     @Override
     public List<Submission> getAllTaskSubmissions() {
-        return null;
+        return submissionRepository.findAll();
     }
 
     @Override
     public List<Submission> getTaskSubmissionsByTaskId(Long taskId) {
-        return null;
+        return submissionRepository.findByTaskId(taskId);
     }
 
     @Override
     public Submission acceptDeclineSubmission(Long id, String status) throws Exception {
-        return null;
+        Submission submission = getTaskSubmissionById(id);
+        submission.setStatus(status);
+        if (status.equals("ACCEPT")) {
+            taskService.completeTask(submission.getTaskId());
+        }
+        return submissionRepository.save(submission);
     }
 }
